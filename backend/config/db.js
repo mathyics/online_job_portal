a68192d745809c51483da973ffb94605987c605c
mathyics <149415979+mathyics@users.noreply.github.com>
2025-11-12 09:13:44 +0530

    Removed credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'yash2006',
-  database: process.env.DB_NAME || 'job_portal_db',
+  password: process.env.DB_PASSWORD || ,
+  database: process.env.DB_NAME || ,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
